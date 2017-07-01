290e1d76794b94aeb6d5ed01e88731d9d49f5973
developer <vivek.aris@gmail.com>
2017-07-01 22:29:32 +0530

    Version 2

---
+++ app/src/main/java/com/iotwebplanet/learn/hostel/Problems.kt
@@ -0,0 +1,16 @@
+package com.iotwebplanet.learn.hostel
+
+/**
+ * Created by developer on 02/07/17.
+ */
+data class Problems(val id:Int,
+val id_num: String,
+val name:String,
+val mobile: String,
+val hostel:String,
+val room_number: String,
+val problem_type:String,
+val description: String,
+val status: Int,
+val com_id: String,
+val created_on: String)
---
+++ app/src/main/java/com/iotwebplanet/learn/hostel/Webservices.kt
@@ -0,0 +1,11 @@
+package com.iotwebplanet.learn.hostel
+
+/**
+ * Created by developer on 02/07/17.
+ */
+
+class Webservices{
+    val ALL_COMPLAINT="http://www.iotwebplanet.com/playground/Hostel/all_problem.php"
+    //val ALL_COMPLAINT="http://192.168.1.5/Hostel/all_prob.php"
+    //val LOGIN_CHECK="http://www.iotwebplanet.com/playground/Hostel/all_problem.php"
+}
